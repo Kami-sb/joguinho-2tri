960af6e90fed30b63f1bd973ff84999b5c756a1a
Kami <103331209+Kami-sb@users.noreply.github.com>
2024-08-20 10:42:26 -0300

    ;;;

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ const perguntas = [
 
                 {
                     texto: "ter tudo o que voce sempre quis na vida em troca de 20 anos de vida",
-                    afirmação: "afirmação",
+                    afirmação: "afirmação1",
                 },
 
                 {
                     texto: "ganhar 20 anos de vida mas não terá tudo que deseja alcançar na vida",
-                    afirmação: "afirmação",
+                    afirmação: "afirmação2",
                 }]
     },
     {
@@ -28,12 +28,12 @@ const perguntas = [
 
                 {
                     texto: "poder eliminar o desmatamento na amazônia e nunca mais poder realizar um sonho seu",
-                    afirmação: "afirmação",
+                    afirmação: "afirmação1",
                 },
 
                 {
                     texto: "poder realizar um sonho na hora que quiser e morrer com o aumento do aquecimento global 1 semana depois  (você não pode desejar acabar com o aquecimento global)",
-                    afirmação: "afirmação",
+                    afirmação: "afirmação2",
                 }]
     },
     {
@@ -43,12 +43,12 @@ const perguntas = [
 
                 {
                     texto: "a tecnologia avançar em um nível as pessoas não precisem fazer esforços físicos porém todos os seus dados e vida pessoal serão expostos ",
-                    afirmação: "afirmação",
+                    afirmação: "afirmação1",
                 },
 
                 {
                     texto: "A tecnologia encontrar a cura para todas as doenças porém começaram a ser criados robôs para exilar a humanidade ",
-                    afirmação: "afirmação",
+                    afirmação: "afirmação2",
                 }]
     },
 ]
